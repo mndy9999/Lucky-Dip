c5aa6ae71875a1bd8a751408ba460ca01cb4eb7f
Oana Furtuna <oana.furtuna98@gmail.com>
2020-03-22 21:28:24 +0000

    Added cards interactions

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CardDisplay : MonoBehaviour
+public class CardDisplay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Card card;
 
@@ -16,9 +17,28 @@ public class CardDisplay : MonoBehaviour
     public Text RollMed;
     public Text RollHigh;
 
+    Vector3 position, defaultPosition;
+    Vector3 scale, defaultScale;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        position = defaultPosition + Vector3.up*70;
+        scale = defaultScale * 1.5f;
+        transform.localScale = scale;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        position = defaultPosition;
+        scale = defaultScale;
+        transform.localScale = scale;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        position = defaultPosition = transform.position;
+        scale = defaultScale = transform.localScale;
         nameText.text = card.Name;
         descriptionText.text = card.Description;
         artworkImage.sprite = card.Artwork;
@@ -48,6 +68,7 @@ public class CardDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, position, 2);
+
     }
 }
